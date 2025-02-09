ab4258081bdbb47ef3ee91dc8b8d7cbaefe28ad1
aliphe <matthias.alif@gmail.com>
2025-02-09 12:17:53 +0100

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import { LinearClient } from "@linear/sdk";
+import { Hono } from "hono";
+
+const app = new Hono();
+const linearClient = new LinearClient({
+  apiKey: process.env.LINEAR_API_KEY || "",
+});
+
+app.post("/sync-issues", async (c) => {
+  const issuesConn = await linearClient.issues();
+  const issues = issuesConn.nodes;
+
+  await Promise.all(
+    issues.map(async (issue) => {
+      await linearClient.updateIssue(issue.id, {
+        addedLabelIds: [issue.id],
+      });
+    }),
+  );
+
+  return c.json(issues);
+});
+
+export default app;
